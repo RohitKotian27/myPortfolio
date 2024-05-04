ffc92fdc52c20fe7b0766185323d3ccf1d958c20
Rohit Kotian <rohitkotian07@gmail.com>
2024-05-04 21:03:23 +0530

    additional fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,14 +34,14 @@ export default function Navbar() {
         }
     ];
     return (
-        <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed px-4 py-2">
+        <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed px-4 py-2 z-10">
             <div className="text-4xl md:text-5xl font-signature ml-2">Rohit</div>
             <ul className="hidden md:flex">
                 {links.map(({ id, link }) => (
                     <li key={id} className="capitalize px-4 cursor-pointer text-gray-400 font-medium hover:scale-105 duration-200"><Link to={link} smooth duration={500}>{link}</Link></li>
                 ))}
             </ul>
-            <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-400 md:hidden">
+            <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 text-gray-400 md:hidden">
                 {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
             </div>
             {nav && (
